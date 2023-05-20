da7a0b6df10ba90ab277e12d564f0ac6aeff6edf
IvanCaceresS <icaceressatorres2001@gmail.com>
2023-05-20 01:22:46 -0400

    latencias

--- Prueba_Rabbit/consumer_elefante/consumer_elefante.py
+++ Prueba_Rabbit/consumer_elefante/consumer_elefante.py
@@ -8,19 +8,25 @@ def consume_messages(queue):
     channel = connection.channel()
     channel.queue_declare(queue=queue)
 
-    def callback(ch, method, properties, body):
-        message = json.loads(body.decode())
-        received_timestamp = time.time()
-        sent_timestamp = message['timestamp']
-        latency = received_timestamp - sent_timestamp
+    # Crear o abrir el archivo de texto para almacenar las latencias
+    with open('latencias.txt', 'a') as file:
 
-        print(f"Mensaje recibido de {queue}: {body}")
-        print(f"Latencia: {latency} segundos")
+        def callback(ch, method, properties, body):
+            message = json.loads(body.decode())
+            received_timestamp = time.time()
+            sent_timestamp = message['timestamp']
+            latency = received_timestamp - sent_timestamp
 
-    channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=True)
+            print(f"Mensaje recibido de {queue}: {body}")
+            print(f"Latencia: {latency} segundos")
 
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
+            # Almacenar la latencia en el archivo de texto
+            file.write(f"{latency}\n")
+
+        channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=True)
+
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        channel.start_consuming()
 
 if __name__ == "__main__":
     queue = "Elefante"
