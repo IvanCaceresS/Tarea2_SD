07c627fdfcf28548c2d53a268f05f176e4b6fc3b
IvanCaceresS <icaceressatorres2001@gmail.com>
2023-05-20 11:52:37 -0400

    imganes comparativas

--- Comparativa/graficos.py
+++ Comparativa/graficos.py
@@ -1,25 +1,37 @@
 import matplotlib.pyplot as plt
 
-# Leer los datos del archivo de texto
-with open('./kafka_latencia_5seg.txt', 'r') as file:
-    data = file.read().splitlines()
-    data = [float(value) for value in data]
+# Leer los datos del primer archivo de texto
+with open('./kafka_latencia_5seg_200k.txt', 'r') as file:
+    data1 = file.read().splitlines()
+    data1 = [float(value) for value in data1]
+
+# Leer los datos del segundo archivo de texto
+with open('./rabbit_latencia_5seg_200k.txt', 'r') as file:
+    data2 = file.read().splitlines()
+    data2 = [float(value) for value in data2]
 
 # Crear una lista de índices para el eje x
-x = list(range(1, len(data) + 1))
+x1 = list(range(1, len(data1) + 1))
+x2 = list(range(1, len(data2) + 1))
 
 # Crear la figura y los ejes del gráfico
 fig, ax = plt.subplots()
 
-# Graficar los datos
-ax.plot(x, data)
+# Graficar los datos del primer archivo (rojo)
+ax.plot(x1, data1, color='red', label='Kafka data pesada, 5 segundos')
+
+# Graficar los datos del segundo archivo (azul)
+ax.plot(x2, data2, color='blue', label='RabbitMq data pesada, 5 segundos')
 
 # Configurar etiquetas del eje x y y
 ax.set_xlabel('N° mensaje')
-ax.set_ylabel('Tiempo(segundos)')
+ax.set_ylabel('Tiempo (segundos)')
 
 # Configurar título del gráfico
 ax.set_title('Gráfico de Datos')
+
+# Agregar leyenda
+ax.legend()
 
 # Mostrar el gráfico
 plt.show()
