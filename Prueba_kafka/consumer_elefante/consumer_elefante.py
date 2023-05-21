6272d4f8a42a2efd80247e22fba740344e4d7548
tobias <tobias.guerrero_c@mail.udp.cl>
2023-05-21 12:51:57 -0400

    tratando de meter redis

--- Prueba_kafka/consumer_elefante/consumer_elefante.py
+++ Prueba_kafka/consumer_elefante/consumer_elefante.py
@@ -2,9 +2,13 @@ from kafka import KafkaConsumer
 import json
 import random
 import time
+import redis
 
 def consume_messages(topic_name):
     consumer = KafkaConsumer(topic_name, bootstrap_servers=['kafka:9092'], value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+
+    redis_client = redis.Redis(host='redis1', port=6379, decode_responses=True)
+    
     with open("latency_kafka.txt", "a") as file:
         for message in consumer:
             received_timestamp = time.time()
@@ -13,7 +17,11 @@ def consume_messages(topic_name):
             file.write(f"{latency}\n")
             print(f"Mensaje recibido de {message.topic}: {message.value}, Latencia: {latency}")
 
+            # Almacenar mensaje y latencia en Redis
+            redis_client.set(message.value, latency)
+
 if __name__ == "__main__":
     topic = "Elefante"
     print("Estoy escuchando el topic ", topic)
     consume_messages(topic)
+
